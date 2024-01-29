4b3af9f992e88113682b20e642feec815d731418
Rujian <roman.2006.08.18@gmail.com>
2024-01-29 20:35:35 +0300

    fix bugs

--- list.c
+++ list.c
@@ -34,7 +34,7 @@ void add_element(List *list, char *word_t){
 void print_list(List *list){
     Item *n_element = list -> head;
     while(n_element){
-        printf("'%s '", n_element -> word);
+        printf("'%s' ", n_element -> word);
         n_element = n_element -> next;
     }
     printf("\n");
@@ -43,7 +43,7 @@ void print_list(List *list){
 void print_output_list(List *list){
     Item *n_element = list ->tail;
     while(n_element){
-        printf("'%s '", n_element -> word);
+        printf("'%s' ", n_element -> word);
         n_element = n_element -> previous;
     }
     printf("\n");
--- main.c
+++ main.c
@@ -9,28 +9,31 @@ void clearInputBuffer() {
 int main(){
     printf("Print input lines\n");
     List *list = list_new();
-    char input;
+    int input;
     create_list(list);
     do{
         printf("Input line:\n\n");
         print_list(list);
         printf("Output line:\n\n");
         print_output_list(list);
-        input = getchar();
+        if(EOF == scanf("%d", &input)){
+                free_list(list);
+                return 0;
+                }
         switch(input){
-            case '1':
+            case 1:
                 printf("Do 1 func\n");
                 break;
-            case '2':
+            case 2:
                 printf("Do 2 func\n");
                 break;
-            case '3':
-               printf("New line\n");
-                  break;
+            case 3:
+                printf("New line\n");
+                break;
             default:
                   printf("Incorrect input\n");
             }
-    }while(input != -1); 
+    }while(1); 
     free_list(list);
     return 0;
 }
